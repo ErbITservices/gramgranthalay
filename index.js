5b723b3292cad846ea7b6f60cc36bef6aa2215b7
ErbITservices <erbitservces@gmail.com>
2024-04-23 11:47:40 +0530

    2024-04-23T11:47:40+05:30

---
+++ index.js
@@ -0,0 +1,49 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+markCommit(-10, 4);
+
+const makeCommits = (n) => {
+  if (n === 0) return simpleGit().push();
+  if (n <= 0) return simpleGit().push();
+  const x = random.int(-35, -9);
+  const y = random.int(1, 6);
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+  console.log(n);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+  });
+};
+
+// makeCommits(20);
